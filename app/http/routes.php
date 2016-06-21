c63ff0fa03a321e51373ae63ccf6a2bdbf2bf9f2
xiaojinhua@sailvan.com <xiaojinhua@sailvan.com>
2016-06-21 10:11:38 +0800

    添加对应的文件夹

---
+++ app/http/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use dobest\Routing\Router as Route;
+
+Route::get('/', 'HomeController@home');
+
+Route::any('foo', function() {
+  echo "Foo!";
+});
+
+Route::filter(function() {
+  return isset($_GET['token']) && $_GET['token'] == 1;
+}, function(){
+  Route::any('bar', function() {
+    echo "Bar!<br>Filter Success!";
+  });
+});
+
+Route::dispatch('View@process');
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => '192.168.200.223',
+    'database'  => 'dobest',
+    'username'  => 'root',
+    'password'  => 'jinhua',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_general_ci',
+    'prefix'    => ''
+]
