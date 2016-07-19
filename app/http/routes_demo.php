22ced039337ea7e197b0ded602cff9bc7e57ab16
roxma <l>
2016-07-19 16:53:47 +0800

    更新 demo

--- app/http/routes_demo.php
+++ app/http/routes_demo.php
@@ -13,14 +13,6 @@ Route::any('/foo', function() {
     echo "Foo!";
 });
 
-Route::filter(function() {
-    return isset($_GET['token']) && $_GET['token'] == 1;
-}, function(){
-  Route::any('bar', function() {
-    echo "Bar!<br>Filter Success!";
-  });
-});
-
 Route::$error_callback = function() {
   throw new Exception("路由无匹配项 404 Not Found");
 };
