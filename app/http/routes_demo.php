96060c2de3f2b55052015884fbeb80c66c48a052
roxma <l>
2016-09-13 17:24:18 +0800

    更新路由策略

--- app/http/routes_demo.php
+++ app/http/routes_demo.php
@@ -13,9 +13,9 @@ Route::get('/',function() {
 });
 
 // login filter
-// { 
+// {
 
-Route::filter('/(:any)/view/(:any)', function($handler) {
+Route::filter('/(:all)/view/(:any)', function($handler) {
     // $sso = new Valsun\Sso("dataSearch");
     // if($sso->checkLogin()==false) { // not login
     //     header('Location: ' . $sso->getSsoUrl());
@@ -28,7 +28,7 @@ Route::filter('/(:any)/view/(:any)', function($handler) {
     return $view;
 });
 
-Route::filter('/(:any)/api/(:any)', function($handler) {
+Route::filter('/(:all)/api/(:any)', function($handler) {
     // $sso = new Valsun\Sso("Transportsys");
     // if($sso->checkLogin()==false) { // not login
     //     echo '{"errCode": -10403, "errMsg":"COMMON_LOGIN_NEEDED"}'; // 未登录公共错误码
@@ -40,30 +40,36 @@ Route::filter('/(:any)/api/(:any)', function($handler) {
 // }
 
 
+function newController($path) {
+    $class = '\\' . implode( '\\',
+        array_map(function($s) {return ucfirst($s);},
+            array_filter(explode('/',$path),'strlen')
+        )
+    ) . 'Controller';
+    return new $class;
+}
+
 // 需要登录的前缀
 // api
 // view
 // 不需要登录，对外开放的 url 前缀
 // openview
 
-Route::any('/(:any)/view/(:any)',function($c,$m) {
-    $class = ucfirst("{$c}Controller");
-    $obj = new $class();
+Route::any('/(:all)/view/(:any)',function($c,$m) {
+    $obj = newController($c);
     $method = "view_$m";
     return $obj->$method();
 });
 
-Route::any('/(:any)/openview/(:any)',function($c,$m) {
-    $class = ucfirst("{$c}Controller");
-    $obj = new $class();
-    $method = "openview_$m";
+Route::any('/(:all)/api/(:any)',function($c,$m) {
+    $obj = newController($c);
+    $method = "api_$m";
     return $obj->$method();
 });
 
-Route::any('/(:any)/api/(:any)',function($c,$m) {
-    $class = ucfirst("{$c}Controller");
-    $obj = new $class();
-    $method = "api_$m";
+Route::any('/(:all)/openview/(:any)',function($c,$m) {
+    $obj = newController($c);
+    $method = "openview_$m";
     return $obj->$method();
 });
 
